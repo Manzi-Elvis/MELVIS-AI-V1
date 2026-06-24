9be1a9d00b9578c66eb6fc3aa4c6fcf1ca726db3
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-06-24 15:51:50 +0200

    voice input

--- melvis/voice_input.py
+++ melvis/voice_input.py
@@ -1,15 +1,18 @@
-import speech_recognition as sr
+# import speech_recognition as sr
+
+# def listen():
+#     recognizer = sr.Recognizer()
+
+#     with sr.Microphone() as source:
+#         print("Listening...")
+#         recognizer.adjust_for_ambient_noise(source)
+
+#         try:
+#             audio = recognizer.listen(source, timeout=5)
+#             text = recognizer.recognize_google(audio)
+#             return text
+#         except:
+#             return ""
 
 def listen():
-    recognizer = sr.Recognizer()
-
-    with sr.Microphone() as source:
-        print("Listening...")
-        recognizer.adjust_for_ambient_noise(source)
-
-        try:
-            audio = recognizer.listen(source, timeout=5)
-            text = recognizer.recognize_google(audio)
-            return text
-        except:
-            return ""
+    return input("You: ")
